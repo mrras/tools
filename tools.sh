bc2d5851448b61b229b67575f44490b2f21ed876
Rama Adhitya Setiadi <50653142+mrras@users.noreply.github.com>
2019-05-15 14:12:35 +0700

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#version 1.0
+
+#MAU NGAPAIN OM?:V
+#RECODE?REEDIT?RECOPYRIGHT?
+#MALU TOLOL!!!
+#KAGA BERMORAL!
+
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet TUAN B4DUT  | lolcat
+
+echo -b "_____________________________________________________________"
+echo -b "Tools : TOOLS INSTALLER $green " |lolcat
+echo -b "AUTHOR : TUAN B4DUT $green " |lolcat
+echo -b "ASSOCIATE : INDONESIAN TERMUX ASSOCIATION $green " |lolcat
+echo -b "ASSOCIATE : INDONESIAN TERMUX TUTORIAL $green " |lolcat
+echo -b "ASSOCIATE : INDONESIAN DARK TERMUX ASSOCIATE $green " |lolcat
+echo -b "NOTE : JELAJAHI SETIAP TOOLS DENGAN BIJAK " | lolcat
+echo -b "_____________________________________________________________"
